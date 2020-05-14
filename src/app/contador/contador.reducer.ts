10de18bf01093b014bc3617d8ed1c27a883d27b3
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-13 23:02:30 -0500

    Acción reset del nieto

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -7,7 +7,8 @@ const _contadorReducer = createReducer(initialState,
   on(actions.incrementar, state => state + 1),
   on(actions.decrementar, state => state - 1),
   on(actions.multiplicar, (state, { numero }) => state * numero),
-  on(actions.dividir, (state, { numero }) => state / numero)
+  on(actions.dividir, (state, { numero }) => state / numero),
+  on(actions.reset, state => state = initialState)
 );
 
 export function contadorReducer(state, action) {
