952e9f014d22290128bb6ca5cde20551beba3916
Jonathan Reátegui <j.reategui2012@gmail.com>
2020-05-13 22:44:56 -0500

    Creación de acciones Multiplicar y Dividir

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,11 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
-import { incrementar, decrementar } from './contador.actions';
+import * as actions from './contador.actions';
 
 export const initialState = 20;
 
 const _contadorReducer = createReducer(initialState,
-  on(incrementar, state => state + 1),
-  on(decrementar, state => state - 1),
+  on(actions.incrementar, state => state + 1),
+  on(actions.decrementar, state => state - 1),
+  on(actions.multiplicar, (state, { numero }) => state * numero),
+  on(actions.dividir, (state, { numero }) => state / numero)
 );
 
 export function contadorReducer(state, action) {
--- src/app/contador/hijo/hijo.component.ts
+++ src/app/contador/hijo/hijo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
+import * as actions from '../contador.actions';
 
 @Component({
   selector: 'app-hijo',
@@ -23,13 +24,11 @@ export class HijoComponent implements OnInit {
   }
 
   multiplicar() {
-    /* this.contador *= 2;
-    this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.multiplicar({ numero: 2 }));
   }
 
   dividir() {
-    /* this.contador /= 2;
-    this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.dividir({ numero: 2 }));
   }
 
 }
